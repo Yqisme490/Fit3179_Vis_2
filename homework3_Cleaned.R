ee2df728b1010ad13e69b19c949cda2f2ab3ec43
Yqisme490 <80311931+Yqisme490@users.noreply.github.com>
2022-10-17 13:58:14 +1100

    Add files via upload

--- homework3_Cleaned.R
+++ homework3_Cleaned.R
@@ -3,6 +3,8 @@ rm(list=ls())
 
 library(tidyverse)
 library(fs)
+library(tibble)
+
 
 #Remove colname special char
 ausDs <- read.csv("AustraliaDataScienceJobs.csv")
@@ -11,34 +13,37 @@ colnames(ausDs)[26:53] <- substr(colnames(ausDs)[26:53],1,nchar(colnames(ausDs)[
 colnames(ausDs)[33] <- "c++"
 write.csv(ausDs,"AustraliaDataScience.csv", row.names = FALSE)
 
-id <- c(0:7)
-State <- c("New South Wales","Victoria","Queensland","South Australia","Western Australia","Tasmania","Northern Territory","Australian Capital Territory")
-keyPair <- data.frame(id, State)
+# Australia Map
+stateEBS <- ausDs %>% group_by(State) %>% summarise(Estimate.Base.Salary = mean(EstimateBaseSalary))
+write.csv(stateEBS,"AustraliaDataScienceEBS.csv", row.names = FALSE)
 
-ausDs <- ausDs %>% group_by(State) %>% summarise(Estimate.Base.Salary = mean(Estimate.Base.Salary))
-write.csv(ausDs,"AustraliaDataScienceEBS.csv", row.names = FALSE)
 #-------------------------------------------------------------------
 countDs <- ausDs %>% group_by(JobTitle) %>% summarise(countVal = length(JobTitle)) %>% arrange(desc(countVal))
 top10Popular <- head(countDs,5)
 top10Popular <- inner_join(ausDs, top10Popular, by = "JobTitle")
 skillRequired <- pivot_longer(top10Popular, cols=26:53, names_to = "skill", values_to = "required")
 skillRequired <- skillRequired[c(1,28,29)]
-#skillRequired  <- skillRequired  %>% group_by(JobTitle,skill) %>% summarise(countVal = sum(required))
 skillRequired <- subset(skillRequired, required != 0)
-write.csv(skillRequired,"top5Skills_2.csv", row.names = FALSE)
+write.csv(skillRequired,"top5Skills.csv", row.names = FALSE)
+
 #-------------------------------------------------------------------
 countDs <- ausDs %>% group_by(CompanyIndustry) %>% summarise(countVal = length(CompanyIndustry)) %>% arrange(desc(countVal))
 countDs <- subset(countDs, CompanyIndustry != '')
-#-------------------------------------------------------------------
-countDs <- ausDs %>% group_by(Job.Title) %>% summarise(countVal = length(Job.Title)) %>% arrange(desc(countVal))
-skillRequired <- pivot_longer(countDs, cols=26:53, names_to = "skill", values_to = "required")
-skillRequired  <- skillRequired  %>% group_by(skill) %>% summarise(countVal = sum(required))
-skillRequired <- subset(skillRequired, countVal != 0)
-colnames(skillRequired) <- c("category","amount")
-write.csv(skillRequired,"importantSkills.csv", row.names = FALSE)
+countDs <- head(countDs,10)
+countDs <- inner_join(ausDs, countDs, by = "CompanyIndustry")
+write.csv(countDs,"top10industry.csv", row.names = FALSE)
 
 #-------------------------------------------------------------------
-ausDs2 <- read.csv("listings2019_2022.csv")
-ausDs2 <- pivot_longer(ausDs2, cols=25:49, names_to = "skill", values_to = "required")
-ausDs2 <- ausDs2 %>% group_by(jobTitle,skill) %>% summarise(countVal = sum(required))
-write.csv(ausDs2,"listings2019_2022_2.csv", row.names = FALSE)
+ds1 <- ausDs[ausDs$CompanyType == 'Company - Public',]
+ds2 <- ausDs[ausDs$CompanyType == 'Company - Private',]
+companyType <- rbind(ds1,ds2)
+companyType <- na.omit(companyType)
+companyType <- companyType %>% group_by(CompanyType) %>% summarise("Company Rating" = mean(CompanyRating),
+                                                                   "Career Opportunities" = mean(CompanyCareerOpportinities),
+                                                                   "Senior Management" = mean(CompanySeniorManagement),
+                                                                   "Compensation and Benefits" = mean(CompensationandBenefits),
+                                                                   "Work Life Balance" = mean(CompanyWorkLifeBalance),
+                                                                   "Culture and Values" = mean(CompanyCultureandValues))
+companyType <- pivot_longer(companyType, cols=2:7, names_to = "category", values_to = "value")
+colnames(companyType)[1] <- "key"
+write.csv(companyType,"companyTypeRating.csv", row.names = FALSE)
--- vega_lite_vis.js
+++ vega_lite_vis.js
@@ -2,6 +2,7 @@ var vg_1 = "homework3.vg.json";
 var vg_2 = "homework4.vg.json";
 var vg_3 = "b.vg.json";
 var vg_4 = "c.vg.json";
+var vg_5 = "a.vg.json";
 
 vegaEmbed("#map", vg_1).then(function(result) {
 }).catch(console.error);
@@ -13,4 +14,7 @@ vegaEmbed("#top10", vg_3).then(function(result) {
 }).catch(console.error);
 
 vegaEmbed("#boxplot", vg_4).then(function(result) {
+}).catch(console.error);
+
+vegaEmbed("#companyType", vg_5).then(function(result) {
 }).catch(console.error);
